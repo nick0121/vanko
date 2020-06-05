b92f52ea1bb1c0217fcc9c2bb23d93ca0eb22aea
nick hartford <skyler0121@gmail.com>
2020-06-05 19:42:15 -0400

    added contact model

--- vanko/pages/models.py
+++ vanko/pages/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+from phone_field import PhoneField
+
 # Create your models here.
+class Contact(models.Model):
+
+    fname = models.CharField(max_length=120)
+    lname = models.CharField(max_length=120)
+    phone = PhoneField(blank=False)
+    email = models.EmailField(unique=True)
+    message = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.fname + " " + self.lname
--- vanko/pages/views.py
+++ vanko/pages/views.py
@@ -4,4 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 
-    pass
+    return render(request, "pages/index.html")
+
+
+def about(request):
+    return render(request, "pages/about.html")
+
+
+def contact(request):
+    return render(request, "pages/contact.html")
